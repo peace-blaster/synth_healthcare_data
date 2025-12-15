a92e05b285a877ede87cf44699d5453043824344
Julian DeVille <53p3n7@gmail.com>
2025-12-14 23:27:24 -0500

    updated sample1 with runtime data

--- sample1.sql
+++ sample1.sql
@@ -1,3 +1,6 @@
+SET STATISTICS IO ON;
+SET STATISTICS TIME ON;
+
 select C.patientId,
 	sum(case when ClaimStatus='paid' then 1 else 0 end) as claims_paid
 from claims.Claims C
@@ -6,4 +9,13 @@ order by claims_paid desc;
 
 /*
 observe that no patient in the database has more than one paid claim
+*/
+
+select count(*) as claims_paid
+from claims.Claims C
+group by C.patientId
+order by claims_paid desc;
+
+/*
+much better way to run it
 */
